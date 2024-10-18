7553cfc42583146745fba1d4c61efeb8bb0b594f
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-18 18:08:48 +0800

    Menambahkan MainActivity dan mengatur onCreate

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -31,17 +31,7 @@ package com.example.questbasiccomposable
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            QuestBasicComposableTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
     }
 }
 
