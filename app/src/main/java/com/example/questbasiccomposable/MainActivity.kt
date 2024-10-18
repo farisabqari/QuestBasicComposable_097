25b2cf0b4c91e7e47d85ec77bc327b1c9b1356ca
farisabqari <127277279+farisabqari@users.noreply.github.com>
2024-10-18 17:59:01 +0800

    struktur dasar Jetpack Compose dengan tema, Scaffold, dan tampilan gambar

--- app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable/MainActivity.kt
@@ -1,17 +1,32 @@
 package com.example.questbasiccomposable
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
+    import com.example.questbasiccomposable.R
+    import android.R.id
+    import android.os.Bundle
+    import androidx.activity.ComponentActivity
+    import androidx.activity.compose.setContent
+    import androidx.activity.enableEdgeToEdge
+    import androidx.compose.foundation.Image
+    import androidx.compose.foundation.layout.Arrangement
+    import androidx.compose.foundation.layout.Column
+    import androidx.compose.foundation.layout.fillMaxSize
+    import androidx.compose.foundation.layout.padding
+    import androidx.compose.material3.Scaffold
+    import androidx.compose.material3.Text
+    import androidx.compose.runtime.Composable
+    import androidx.compose.ui.Alignment
+    import androidx.compose.ui.Modifier
+    import androidx.compose.ui.graphics.Color
+    import androidx.compose.ui.res.painterResource
+    import androidx.compose.ui.text.font.FontFamily
+    import androidx.compose.ui.text.font.FontWeight
+    import androidx.compose.ui.tooling.preview.Preview
+    import androidx.compose.ui.unit.sp
+    import androidx.compose.ui.unit.dp
+    import androidx.compose.foundation.shape.RoundedCornerShape
+    import androidx.compose.foundation.border
+    import androidx.compose.foundation.layout.size
+    import com.example.questbasiccomposable.ui.theme.QuestBasicComposableTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
